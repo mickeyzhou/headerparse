7917e879be62ac1640a37135a3f341757b2261aa
mickeyzhou <305816643@qq.com>
2017-03-29 08:54:42 +0000

    modify app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
-    res.send('hello');    
+    res.send('https://mickeyzhou.herokuapp.com/api/whoami');    
 });
 
 app.get('/api/whoami', function(req, res){
